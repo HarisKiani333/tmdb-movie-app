42ea0b10a8fafed3192dcbdf17a6b2e2948e3f7b
HarisKiani333 <65072289+HarisKiani333@users.noreply.github.com>
2025-07-03 16:41:31 +0500

    Fixing Reference Clutter

---
+++ frontend/src/services/api.js
@@ -0,0 +1,27 @@
+const API_KEY = 'fb7bb23f03b6994dafc674c074d01761';
+const BASE_URL = 'https://api.themoviedb.org/3';
+
+
+
+
+export const GetPopularMovies = async () => {
+    const url = `${BASE_URL}/movie/popular?api_key=${API_KEY}`;
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error('Failed to fetch favorite movies') ;
+    }
+    const data = await response.json();
+    return data.results;
+}
+
+export const fetchMovies = async (query) => {
+    const url = `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`;
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error('Failed to fetch movies');
+    }
+    const data = await response.json();
+    return data.results;
+}
+
+
